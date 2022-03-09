7f21baf0d292152073b5f9dbf0ac158ff55e0d73
BluBack <kangrokristo1@gmail.com>
2022-03-09 12:24:04 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,14 @@
+import sys
+import pygame
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Game")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
