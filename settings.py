c4018c7fceeb62a367a6a125813d3366ce940e35
BluBack <kangrokristo1@gmail.com>
2022-03-23 12:34:27 +0300

    game

--- settings.py
+++ settings.py
@@ -6,13 +6,13 @@ class Settings:
         # screen settings
         self.screen_width = 800
         self.screen_height = 600
-        self.bg_color = (238,238,228)
+        self.bg_color = (20,20,69)
         # ship settings
         self.ship_limit = 3
         # bullet settings
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 64, 64, 64
+        self.bullet_color = 255, 255, 255
         self.bullets_allowed = 3
         # aliens settings
         self.fleet_drop_speed = 5
